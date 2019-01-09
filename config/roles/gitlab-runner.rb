38018c98a3603e35d74cc2bda216738ae037dd79
AlessandroCaetano <alessandro.caetanob@gmail.com>
2019-01-09 14:56:25 -0200

    Mudança nos role de gitlab-runner para ter a runlist completa

--- config/roles/gitlab-runner.rb
+++ config/roles/gitlab-runner.rb
@@ -1,9 +1,4 @@
+
 name "gitlab-runner"
 description "Install and configure gitlab runner server"
-
-run_list = [
-  'recipe[basics]',
-  'recipe[docker]',
-  'recipe[firewalld]',
-  'recipe[gitlab-ci-runner]'
-]
+run_list "recipe[basics]", "recipe[docker]", "recipe[firewalld]", "recipe[gitlab-ci-runner]"
