d920c49d95b5c0b821cbee29be5c9c2391353164
Alessandro Caetano Beltrão <alessandro.caetanob@gmail.com>
2019-01-25 15:54:34 -0200

    Adicionando mais runners

--- cookbooks/gitlab-ci-runner/recipes/default.rb
+++ cookbooks/gitlab-ci-runner/recipes/default.rb
@@ -18,7 +18,7 @@ include_recipe "#{cookbook_name}::repository"
 include_recipe "#{cookbook_name}::package"
 include_recipe "#{cookbook_name}::service"
 
-$CHAKE_ENV = ENV.fetch('CHAKE_ENV', 'local')
+$CHAKE_ENV = ENV.fetch('CHAKE_ENV', 'ed')
 
 $ROOT_DIR = File.expand_path('../../../../../.', __FILE__)
 
